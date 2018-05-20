41c77148a18490e7f5467476eb4aa56797957fea
William Michael Holbrook II <wmh@fastmail.com>
2018-05-20 00:39:10 -0500

    Add initial library and testbench structure

---
+++ NwsApiTestbench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NwsApiTestbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
