e01a9fca3df88b8a0b9ca9c41c29989c7b7cd4da
William Michael Holbrook II <wmh@fastmail.com>
2018-05-20 06:29:52 -0500

    Add initial API endpoints for consumers
    The consumers should have a relatively sane API to call without
    needing to deal with the complexity of some of the NWS API endpoint
    data expecations.
    
    Codifying this first so that we have a view of what consumers will
    need to provide will probably lead to better results as we're not
    working strictly as a passthrough from NWS API to consumer input.

---
+++ NwsApi/NwsLoader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+
+namespace NwsApi
+{
+    /// <summary>
+    /// Loads information from the National Weather Service
+    /// </summary>
+    /// <see href="https://forecast-v3.weather.gov/documentation"/>
+    // TODO: Rename this class to a better "this is an API consumers should use" name
+    public class NwsLoader
+    {
+        #region Constructors
+
+        public NwsLoader() : this(new HttpClient())
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor to use if consumers have premade an HttpClient
+        /// </summary>
+        /// <param name="httpClient"></param>
+        public NwsLoader(HttpClient httpClient)
+        {
+            this.HttpClient = httpClient;
+        }
+
+        #endregion
+
+        #region Properties
+
+        // TODO: Remove direct reliance on HttpClient so we can write tests that parse pre-selected responses
+        private HttpClient HttpClient { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Wrapper for /points/{latitude,longitude}
+        ///
+        /// https://forecast-v3.weather.gov/documentation
+        /// </summary>
+        /// <param name="latitude">EPSG:4326 latitude</param>
+        /// <param name="longitude">EPSG:4326 longitude</param>
+        /// <returns>NwsPoint containig information about the specified point</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public NwsPoint PointData(double latitude, double longitude)
+        {
+            throw NotImplementedException(nameof(PointData) + " not implemented");
+        }
+
+        /// <summary>
+        /// Wrapper for /gridpoints/{wfo}/{x},{y} API endpoint
+        ///
+        /// https://forecast-v3.weather.gov/documentation
+        /// </summary>
+        /// <param name="weatherForecastOfficeId">Weather Forecast Office identifier. https://en.wikipedia.org/wiki/List_of_National_Weather_Service_Weather_Forecast_Offices</param>
+        /// <param name="x">x coordinate on the grid</param>
+        /// <param name="y">y coordinate on the grid</param>
+        /// <returns>NwsGridpoint containing information about the grid at x,y</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public NwsGridpoint GridpointData(string weatherForecastOfficeId, int x, int y)
+        {
+            throw NotImplementedException(nameof(GridpointData) + " not implemented");
+        }
+
+        #endregion
+
+    }
+}
---
+++ NwsApi/NwsPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NwsApi
+{
+    public class NwsPoint
+    {
+        [JsonProperty]
+        public Uri ForecastOffice { get; set; }
+        
+        [JsonProperty]
+        public Uri Forecast { get; set; }
+
+        [JsonProperty]
+        public Uri ForecastHourly { get; set; }
+
+        [JsonProperty]
+        public Uri ForecastGridData { get; set; }
+
+        [JsonProperty]
+        public Uri ObservationStations { get; set; }
+
+        [JsonProperty]
+        public Uri ForecastZone { get; set; }
+
+        [JsonProperty]
+        public Uri County { get; set; }
+
+        [JsonProperty]
+        public Uri FireWeatherZone { get; set; }
+
+        [JsonProperty]
+        public string TimeZone { get; set; }
+
+        [JsonProperty]
+        public string RadarStation { get; set; }
+    }
+}
